2edb9df07af7d9546f60e4b4749e149169ce544c
Hossam Hassan <99551852+HossamHassan70@users.noreply.github.com>
2024-03-29 01:58:13 +0200

    Fix bug in main file

--- main.c
+++ main.c
@@ -1,19 +1,9 @@
 #include "monty.h"
 #include <stdio.h>
-bus_t bus = {NULL, NULL, NULL, 0};
-/**
- * Function: main
- * ----------------------------
- * Entry point of the Monty bytecode interpreter.
- *
- * @argc: Number of command-line arguments.
- * @argv: Array of command-line arguments.
- *
- * Returns: 0 on successful execution.
- */
 
-int main(int argc, char *argv[])
-{
+bus_t bus = {NULL, NULL, NULL, 0};
+
+int main(int argc, char *argv[]) {
     FILE *file = fopen(argv[1], "r");
     stack_t *stack = NULL;
     char *line = NULL;
@@ -21,38 +11,36 @@ int main(int argc, char *argv[])
     ssize_t read;
     unsigned int line_number = 0;
 
-    if (argc != 2)
-    {
+    if (argc != 2) {
+        /* Print usage message */
         fprintf(stderr, "Usage: %s file\n", argv[0]);
-        return EXIT_FAILURE;
+        return 1;
     }
 
-    if (!file)
-    {
+    if (!file) {
+        /* Print error message if file cannot be opened */
         fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-        return EXIT_FAILURE;
+        return 1;
     }
 
-    while ((read = getline(&line, &len, file)) != -1)
-    {
+    while ((read = getline(&line, &len, file)) != -1) {
         line_number++;
 
         /* Remove trailing newline if present */
         if (line[read - 1] == '\n')
             line[read - 1] = '\0';
 
-        if (execute(line, &stack, line_number, file) == EXIT_FAILURE)
-        {
+        if (execute(line, &stack, line_number, file) == EXIT_FAILURE) {
             /* Handle execution failure */
             fclose(file);
             free(line);
             free_stack(stack);
-            return EXIT_FAILURE;
+            return 1;
         }
     }
 
     fclose(file);
     free(line);
     free_stack(stack);
-    return EXIT_SUCCESS;
+    return 0;
 }
